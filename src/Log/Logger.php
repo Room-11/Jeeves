88b855f8d674f05e9a3ff89786a3f9f421494998
Pieter Hordijk <info@pieterhordijk.com>
2016-01-12 20:24:58 +0100

    Added extra data param

--- src/Log/Logger.php
+++ src/Log/Logger.php
@@ -4,5 +4,5 @@ namespace Room11\Jeeves\Log;
 
 interface Logger
 {
-    public function log(int $level, string $message);
+    public function log(int $level, string $message, $extraData = null);
 }
