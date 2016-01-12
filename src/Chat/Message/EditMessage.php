6aeaa7d48075e511977fc535383647509540b4ad
Pieter Hordijk <info@pieterhordijk.com>
2016-01-12 21:00:45 +0100

    Added edit message message

---
+++ src/Chat/Message/EditMessage.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types=1);
+
+namespace Room11\Jeeves\Chat\Message;
+
+class EditMessage implements Message
+{
+    private $id;
+
+    private $userId;
+
+    private $username;
+
+    private $roomId;
+
+    private $content;
+
+    private $numberOfEdits = 0;
+
+    private $timestamp;
+
+    public function __construct(array $data)
+    {
+        $this->id            = $data['id'];
+        $this->userId        = $data['user_id'];
+        $this->username      = $data['user_name'];
+        $this->roomId        = $data['room_id'];
+        $this->content       = $data['content'];
+        $this->numberOfEdits = $data['message_edits'];
+        $this->timestamp     = new \DateTime('@' . $data['time_stamp']);
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getRoomId(): int
+    {
+        return $this->roomId;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function getNumberOfEdits(): int
+    {
+        return $this->numberOfEdits;
+    }
+
+    public function getTimestamp(): \DateTime
+    {
+        return $this->timestamp;
+    }
+}
--- src/Chat/Message/Factory.php
+++ src/Chat/Message/Factory.php
@@ -12,6 +12,10 @@ class Factory
             return new NewMessage($message['e'][0]);
         }
 
+        if (isset($message['e']) && $message['e'][0]['event_type'] === 2) {
+            return new EditMessage($message['e'][0]);
+        }
+
         return new Unknown($message);
     }
 }
