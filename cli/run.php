0f07807e98fa9e88d7bea254a3c6edd5e40a5cb1
Pieter Hordijk <info@pieterhordijk.com>
2016-01-16 16:23:37 +0100

    Cleaned up code

--- cli/run.php
+++ cli/run.php
@@ -6,34 +6,31 @@ use Amp\Artax\Client as HttpClient;
 use Room11\Jeeves\Fkey\Retriever as FkeyRetriever;
 use Room11\Jeeves\OpenId\Client;
 
-use Room11\Jeeves\Chat\Plugin\Collection as CommandCollection;
+use Room11\Jeeves\Chat\Plugin\Collection as PluginCollection;
 use Room11\Jeeves\Chat\Message\Factory as MessageFactory;
 
-use Room11\Jeeves\Chat\Plugin\Version as VersionCommand;
-use Room11\Jeeves\Chat\Plugin\Urban as UrbanCommand;
-use Room11\Jeeves\Chat\Plugin\Wikipedia as WikipediaCommand;
+use Room11\Jeeves\Chat\Plugin\Version as VersionPlugin;
+use Room11\Jeeves\Chat\Plugin\Urban as UrbanPlugin;
+use Room11\Jeeves\Chat\Plugin\Wikipedia as WikipediaPlugin;
 
 use Amp\Websocket\Handshake;
 use Room11\Jeeves\WebSocket\Handler;
 
 require_once __DIR__ . '/../bootstrap.php';
 
-$httpClient   = new HttpClient();
-
+$httpClient    = new HttpClient();
 $fkeyRetriever = new FkeyRetriever($httpClient);
-
-$openIdClient = new Client($openIdCredentials, $httpClient, $fkeyRetriever);
+$openIdClient  = new Client($openIdCredentials, $httpClient, $fkeyRetriever);
 
 $openIdClient->logIn();
 
-$chatKey = $fkeyRetriever->get('http://chat.stackoverflow.com/rooms/' . $roomId . '/php');
-
+$chatKey      = $fkeyRetriever->get('http://chat.stackoverflow.com/rooms/' . $roomId . '/php');
 $webSocketUrl = $openIdClient->getWebSocketUri($roomId);
 
-$commands = (new CommandCollection())
-    ->register(new VersionCommand($httpClient, $chatKey))
-    ->register(new UrbanCommand($httpClient, $chatKey))
-    ->register(new WikipediaCommand($httpClient, $chatKey))
+$commands = (new PluginCollection())
+    ->register(new VersionPlugin($httpClient, $chatKey))
+    ->register(new UrbanPlugin($httpClient, $chatKey))
+    ->register(new WikipediaPlugin($httpClient, $chatKey))
 ;
 
 \Amp\run(function () use ($webSocketUrl, $commands, $logger) {
