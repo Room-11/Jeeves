2579e4fc1cff64c055281d9bd9b8c8a4b0d0024e
Pieter Hordijk <info@pieterhordijk.com>
2016-01-16 16:51:42 +0100

    Created chat client

---
+++ src/Chat/Client/Xhr.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace Room11\Jeeves\Chat\Client;
+
+use Amp\Artax\Client;
+use Amp\Artax\FormBody;
+use Amp\Artax\Request;
+
+class Xhr
+{
+    private $httpClient;
+
+    private $fkey;
+
+    private $roomId;
+
+    public function __construct(Client $httpClient, string $fkey, int $roomId)
+    {
+        $this->httpClient = $httpClient;
+        $this->fkey       = $fkey;
+        $this->roomId     = $roomId;
+    }
+
+    public function request(string $uri): \Generator
+    {
+        yield $this->httpClient->request($uri);
+    }
+
+    public function postMessage(string $text): \Generator
+    {
+        $body = (new FormBody)
+            ->addField('text', $text)
+            ->addField('fkey', $this->fkey)
+        ;
+
+        $request = (new Request)
+            ->setUri('http://chat.stackoverflow.com/chats/' . $this->roomId . '/messages/new')
+            ->setMethod('POST')
+            ->setBody($body)
+        ;
+
+        $promise = $this->httpClient->request($request);
+
+        yield $promise;
+    }
+}
