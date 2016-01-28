65e2d2def6683af56f3e7a9783ea45ff8c807447
Pieter Hordijk <info@pieterhordijk.com>
2016-01-28 23:07:31 +0100

    First "working" 3v4l fetcher

--- src/Chat/Plugin/EvalCode.php
+++ src/Chat/Plugin/EvalCode.php
@@ -2,15 +2,19 @@
 
 namespace Room11\Jeeves\Chat\Plugin;
 
-use Room11\Jeeves\Chat\Client\Xhr as ChatClient;
+use Room11\Jeeves\Chat\Client\ChatClient;
 use Room11\Jeeves\Chat\Command\Command;
 use Room11\Jeeves\Chat\Command\Message;
 use Amp\Artax\FormBody;
 use Amp\Artax\Request;
+use Amp\Pause;
 
 class EvalCode implements Plugin
 {
     const COMMAND = 'eval';
+
+    // limit the number of requests while polling for results
+    const REQUEST_LIMIT = 20;
 
     private $chatClient;
 
@@ -26,8 +30,6 @@ class EvalCode implements Plugin
         }
 
         yield from $this->getResult($message);
-
-        var_dump('HIIIIIIIIIIIIIIIIIIIIIT');
     }
 
     private function validMessage(Message $message): bool
@@ -39,9 +41,15 @@ class EvalCode implements Plugin
 
     private function getResult(Message $message): \Generator
     {
+        $code = html_entity_decode(implode(' ', $message->getParameters()), ENT_QUOTES);
+
+        if (strpos($code, '<?php') === false) {
+            $code = '<?php ' . $code;
+        }
+
         $body = (new FormBody)
             ->addField('title', '')
-            ->addField('code', '<?php var_dump(\'foo\');')
+            ->addField('code', $code)
         ;
 
         $request = (new Request)
@@ -52,16 +60,74 @@ class EvalCode implements Plugin
 
         $response = yield from $this->chatClient->request($request);
 
-        var_dump($response);
+        // 3v4l uses only paths for redirects
+        $result = yield from $this->pollUntilDone(
+            'https://3v4l.org' . $response->getPreviousResponse()->getHeader('Location')[0]
+        );
+
+        yield from $this->chatClient->postMessage(
+            $this->getMessage($result, $response->getPreviousResponse()->getHeader('Location')[0])
+        );
     }
 
-    private function getMessage(array $result): string
+    /**
+     * 3v4l responds with full HTML pages while polling #idonteven
+     *
+     * It seems like there is a class `busy` when the result is not complete yet and a class `done` when it is finished
+     * somewhere in the html. inb4 this method breaks when you look at it the wrong way or Jon changes something
+     */
+    private function pollUntilDone(string $snippetUrl): \Generator
+    {
+        $requests = 0;
+
+        while (true && $requests <= self::REQUEST_LIMIT) {
+            $requests++;
+
+            $result = yield from $this->chatClient->request($snippetUrl);
+
+            $useInternalErrors = libxml_use_internal_errors(true);
+
+            $dom = new \DOMDocument();
+            $dom->loadHTML($result->getBody());
+
+            libxml_use_internal_errors($useInternalErrors);
+
+            $xpath = new \DOMXPath($dom);
+
+            $nodes = $xpath->query("//*[contains(concat(\" \", normalize-space(@class), \" \"), \" busy \")]");
+
+            if (!$nodes->length) {
+                return $this->parseResponse($result->getBody());
+            }
+
+            yield new Pause(2500);
+        }
+    }
+
+    private function parseResponse(string $body): array
+    {
+        $useInternalErrors = libxml_use_internal_errors(true);
+
+        $dom = new \DOMDocument();
+        $dom->loadHTML($body);
+
+        libxml_use_internal_errors($useInternalErrors);
+
+        $resultPane = $dom->getElementById('tab')->getElementsByTagName('dl')->item(0);
+
+        return [
+            'title'  => $resultPane->getElementsByTagName('dt')->item(0)->textContent,
+            'result' => $resultPane->getElementsByTagName('dd')->item(0)->textContent,
+        ];
+    }
+
+    private function getMessage(array $result, string $url): string
     {
         return sprintf(
             '[ [%s](%s) ] %s',
-            $result['list'][0]['word'],
-            $result['list'][0]['permalink'],
-            str_replace("\r\n", ' ', $result['list'][0]['definition'])
+            $result['title'],
+            'https://3v4l.org' . $url,
+            $result['result']
         );
     }
 }
