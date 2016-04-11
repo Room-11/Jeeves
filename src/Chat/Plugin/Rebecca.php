b658d54d305d57dc149a1396bf2452f3f3833768
gooh <gordon.oheim@instana.com>
2016-04-11 16:34:32 +0200

    Add Rebecca command
    Adds a Rebecca command. Will post the video link to Friday on Fridays.
    Will give appropriate messages on every other day than Friday.

---
+++ src/Chat/Plugin/Rebecca.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace Room11\Jeeves\Chat\Plugin;
+
+use Room11\Jeeves\Chat\Client\ChatClient;
+use Room11\Jeeves\Chat\Command\Command;
+use Room11\Jeeves\Chat\Command\Message;
+
+class Rebecca implements Plugin
+{
+    const COMMAND = 'rebecca';
+
+    const VIDEO_URL = 'https://www.youtube.com/watch?v=kfVsfOSbJY0';
+
+    private $chatClient;
+
+    public function __construct(ChatClient $chatClient) {
+        $this->chatClient = $chatClient;
+    }
+
+    public function handle(Message $message): \Generator {
+        if (!$this->validMessage($message)) {
+            return;
+        }
+
+        yield from $this->getResult($message);
+    }
+
+    private function validMessage(Message $message): bool {
+        return $message instanceof Command
+            && $message->getCommand() === self::COMMAND;
+    }
+
+    private function getResult(Message $message): \Generator {
+        yield from $this->chatClient->postMessage(
+            sprintf(
+                ':%s %s',
+                $message->getOrigin(),
+                $this->getRebeccaLinkIfFriday()
+            )
+        );
+    }
+
+    private function getRebeccaLinkIfFriday(): string
+    {
+        switch (date('l')) {
+            case 'Thursday':
+                return "Happy Prebeccaday!";
+            case 'Friday':
+                return self::VIDEO_URL;
+            case 'Saturday':
+                return "Today is Saturday. And Sunday comes afterwards";
+            default:
+                return "Dude, it's not Rebeccaday yet!";
+        }
+    }
+}
