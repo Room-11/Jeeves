ecb0a3e8bf3241615d56e9846b3a61d2bf51cced
Gourab Nag <gourabnag12@gmail.com>
2016-04-26 13:26:08 +0530

    Fixed the issues, implemented an handler if the message char length, exceeds 500

--- src/Chat/Plugin/Google.php
+++ src/Chat/Plugin/Google.php
@@ -1,9 +1,12 @@
 <?php  declare(strict_types = 1);
+
 namespace Room11\Jeeves\Chat\Plugin;
+
 use Amp\Artax\Response;
 use Room11\Jeeves\Chat\Client\ChatClient;
 use Room11\Jeeves\Chat\Command\Command;
 use Room11\Jeeves\Chat\Command\Message;
+
 class Google implements Plugin {
     const COMMAND = "google";
     private $chatClient;
@@ -13,34 +16,41 @@ class Google implements Plugin {
         $this->chatClient = $chatClient;
         $this->bitlyAccessToken = $bitlyAccessToken;
     }
+
     public function handle(Message $message): \Generator {
         if (!$this->validMessage($message)) {
             return;
         }
         yield from $this->getResult($message);
     }
+
     private function validMessage(Message $message): bool {
         return $message instanceof Command
             && $message->getCommand() === self::COMMAND
             && $message->getParameters();
     }
+
     private function getResult(Message $message): \Generator {
         $uri = "https://www.google.com/search?q=" . urlencode(implode(' ', $message->getParameters()));
         /** @var Response $response */
         $response = yield from $this->chatClient->request($uri);
+
         if ($response->getStatus() !== 200) {
             yield from $this->chatClient->postMessage(
                 "It was Google's fault, not mine."
             );
             return;
         }
+
         $internalErrors = libxml_use_internal_errors(true);
         $dom = new \DOMDocument();
-        $google = $response->getBody();
+        $google = utf8_encode($response->getBody());
         $dom->loadHTML($google);
         libxml_use_internal_errors($internalErrors);
         $xpath = new \DOMXPath($dom);
+
         $nodes = $xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' g ')]");
+
         if($nodes->length === 0) {
             yield from $this->chatClient->postMessage(sprintf(
                 ":%s Did you know? That `%s...` dosen't exist in the world! Cuz' GOOGLE can't find it :P",
@@ -49,12 +59,15 @@ class Google implements Plugin {
             ));
             return;
         }
+
         $length = min(3, $nodes->length);
         $toPostMessage = "";
+
         for($i = 0; $i < $length; $i++) {
-            start_of_loop:
+            var_dump($toPostMessage);
+
             $currentNode = $nodes[$i];
-            $nodeLink= $xpath->query("//h3/a", $currenNode);
+            $nodeLink= $xpath->query("//h3/a", $currentNode);
             $nodeLinkText = $nodeLink->item($i)->textContent;
             $nodeLink = $nodeLink->item($i)->getAttribute("href");
             $nodeDescription = substr(strip_tags(nl2br($xpath->query('//span[@class="st"]', $currentNode)->item($i)->textContent)), 0, 55);
@@ -62,24 +75,43 @@ class Google implements Plugin {
                 continue;
             }
             $link = $matches[1];
+
             $apiUri = sprintf(
                 "https://api-ssl.bitly.com/v3/shorten?access_token=%s&longUrl=%s",
                 $this->bitlyAccessToken,
                 $link
             );
+
             $shortener = yield from $this->chatClient->request($apiUri);
             $shortened = json_decode($shortener->getBody(), true);
             $shortenedLink = $shortened["data"]["url"];
-            $toPostMessage .= sprintf(
+            $toAppendMessage = sprintf(
                 "  **[%s](%s)** %s...  |",
-                utf8_encode($nodeLinkText),
+                $nodeLinkText,
                 $shortenedLink,
                 $nodeDescription
             );
+            if(strlen($toPostMessage) + strlen($toAppendMessage) > 500) {
+                continue;
+            }
+            $toPostMessage .= $toAppendMessage;
         }
-        $toPostMessage .= "  **[Google Search Url]($uri)**";
+
+        $apiUri = sprintf(
+            "https://api-ssl.bitly.com/v3/shorten?access_token=%s&longUrl=%s",
+            $this->bitlyAccessToken,
+            $uri
+        );
+        $googleSearchBitlyLink = yield from $this->chatClient->request($apiUri);
+        $googleSearchBitlyLink = json_decode($googleSearchBitlyLink->getBody(), true)["data"]["url"];
+        if(strlen($toPostMessage) + strlen("  **[Search Url]($googleSearchBitlyLink)**") > 500) {
+            $toPostMessage = substr($toPostMessage, 0, 500 - (strlen("  **[Search Url]($googleSearchBitlyLink)**") + 1));
+            $toPostMessage .= "|";
+        }
+        $toPostMessage .= "  **[Search Url]($googleSearchBitlyLink)**";
         $toPostMessage = str_replace("\r", " ", $toPostMessage);
         $toPostMessage = str_replace("\r\n", " ", $toPostMessage);
+        var_dump($toPostMessage);
         yield from $this->chatClient->postMessage(str_replace("\n", " ", $toPostMessage));
     }
 }
