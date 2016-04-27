2ee442fda6875b6dd584d78c1e14da2615bebc64
Chris Wright <daverandom@php.net>
2016-04-27 16:19:00 +0100

    Add special case support and some special cases

--- src/Chat/Plugin/Docs.php
+++ src/Chat/Plugin/Docs.php
@@ -19,6 +19,76 @@ class Docs implements Plugin
 
     private $chatClient;
 
+    private $specialCases = [
+        '::' => '[`::`](' . self::MANUAL_URL_BASE . '/language.oop5.paamayim-nekudotayim.php) is the scope resolution operator',
+        '$_cookie' => '[`$_COOKIE`](' . self::MANUAL_URL_BASE . '/reserved.variables.cookie.php)',
+        '$_env' => '[`$_ENV`](' . self::MANUAL_URL_BASE . '/reserved.variables.env.php)',
+        '$_files' => '[`$_FILES`](' . self::MANUAL_URL_BASE . '/reserved.variables.files.php)',
+        '$_get' => '[`$_GET`](' . self::MANUAL_URL_BASE . '/reserved.variables.get.php)',
+        '$_post' => '[`$_POST`](' . self::MANUAL_URL_BASE . '/reserved.variables.post.php)',
+        '$_request' => '[`$_REQUEST`](' . self::MANUAL_URL_BASE . '/reserved.variables.request.php)',
+        '$_server' => '[`$_SERVER`](' . self::MANUAL_URL_BASE . '/reserved.variables.server.php)',
+        '$_session' => '[`$_SESSION`](' . self::MANUAL_URL_BASE . '/reserved.variables.session.php)',
+        'abstract' => '[Class abstraction](' . self::MANUAL_URL_BASE . '/language.oop5.abstract.php)',
+        'arrays' => '[Arrays](' . self::MANUAL_URL_BASE . '/language.types.array.php)',
+        'autoloading' => '[Autoloading Classes](' . self::MANUAL_URL_BASE . '/language.oop5.autoload.php)',
+        'bools' => '[Booleans](' . self::MANUAL_URL_BASE . '/language.types.boolean.php)',
+        'booleans' => '[Booleans](' . self::MANUAL_URL_BASE . '/language.types.boolean.php)',
+        'callables' => '[Callbacks / Callables](' . self::MANUAL_URL_BASE . '/language.types.callable.php)',
+        'casts' => '[Type juggling](' . self::MANUAL_URL_BASE . '/language.types.type-juggling.php)',
+        'class' => '[Classes and objects](' . self::MANUAL_URL_BASE . '/language.oop5.php) (Object-oriented programming)',
+        'clone' => '[Object cloning](' . self::MANUAL_URL_BASE . '/language.oop5.cloning.php)',
+        'context' => '[Stream context options and parameters](' . self::MANUAL_URL_BASE . '/context.php',
+        'const' => '`const` can be used to define [constants](' . self::MANUAL_URL_BASE . '/language.constants.syntax.php)'
+                 . ' and [class constants](' . self::MANUAL_URL_BASE . '/language.oop5.constants.php)',
+        'errors' => '[Error handling](' . self::MANUAL_URL_BASE . '/language.errors.php)',
+        'exceptions' => '[Exceptions](' . self::MANUAL_URL_BASE . '/language.exceptions.php)',
+        'extends' => '[OOP inheritance](' . self::MANUAL_URL_BASE . '/language.oop5.inheritance.php)',
+        'final' => '[`final` keyword](' . self::MANUAL_URL_BASE . '/language.oop5.final.php)',
+        'floats' => '[Floating point numbers](' . self::MANUAL_URL_BASE . '/language.types.float.php)',
+        'functions' => '[Functions](' . self::MANUAL_URL_BASE . '/language.functions.php)',
+        'generators' => '[Generators](' . self::MANUAL_URL_BASE . '/language.generators.php)',
+        'global' => 'Global ---All--- None Of The Things!',
+        'hints' => '[Type hinting](' . self::MANUAL_URL_BASE . '/language.oop5.typehinting.php)',
+        'ints' => '[Integers](' . self::MANUAL_URL_BASE . '/language.types.integer.php)',
+        'implements' => '[Interfaces](' . self::MANUAL_URL_BASE . '/language.oop5.interfaces.php)',
+        'integers' => '[Integers](' . self::MANUAL_URL_BASE . '/language.types.integer.php)',
+        'interfaces' => '[Interfaces](' . self::MANUAL_URL_BASE . '/language.oop5.interfaces.php)',
+        'javascript' => 'I think you\'re in the [wrong room](http://chat.stackoverflow.com/rooms/17/javascript).',
+        'magic' => 'PHP was designed by wizards and so uses magic extensively.'
+                 . ' [Magic constants](' . self::MANUAL_URL_BASE . '/language.constants.predefined.php) and'
+                 . ' [magic methods](' . self::MANUAL_URL_BASE . '/language.oop5.magic.php) are both available.',
+        'namespaces' => '[Namespaces](' . self::MANUAL_URL_BASE . '/language.namespaces.php)',
+        'null' => '[NULL](' . self::MANUAL_URL_BASE . '/language.types.null.php)',
+        'objects' => '[Objects](' . self::MANUAL_URL_BASE . '/language.types.object.php)',
+        'oop' => '[Classes and objects](' . self::MANUAL_URL_BASE . '/language.oop5.php) (Object-oriented programming)',
+        'operators' => '[Operators](' . self::MANUAL_URL_BASE . '/language.operators.php)',
+        'refs' => '[References Explained](' . self::MANUAL_URL_BASE . '/language.references.php)',
+        'references' => '[References Explained](' . self::MANUAL_URL_BASE . '/language.references.php)',
+        'resources' => '[Resources](' . self::MANUAL_URL_BASE . '/language.types.resource.php)',
+        'return' => '[Returning values](' . self::MANUAL_URL_BASE . '/functions.returning-values.php)',
+        'precedence' => '[Operator precedence](' . self::MANUAL_URL_BASE . '/language.operators.precedence.php)',
+        'private' => '[Class member visibility](' . self::MANUAL_URL_BASE . '/language.oop5.visibility.php)',
+        'properties' => '[Object Properties](' . self::MANUAL_URL_BASE . '/language.oop5.properties.php)',
+        'propertys' => '[Object Properties](' . self::MANUAL_URL_BASE . '/language.oop5.properties.php)',
+        'protected' => '[Class member visibility](' . self::MANUAL_URL_BASE . '/language.oop5.visibility.php)',
+        'public' => '[Class member visibility](' . self::MANUAL_URL_BASE . '/language.oop5.visibility.php)',
+        'scopes' => '[Variable scope](' . self::MANUAL_URL_BASE . '/language.variables.scope.php)',
+        'static' => 'The `static` keyword can be used to create [static class members](' . self::MANUAL_URL_BASE . '/language.oop5.static.php)'
+                  . ' and [static variables](' . self::MANUAL_URL_BASE . '/language.variables.scope.php#language.variables.scope.static).'
+                  . ' In general, you shouldn\'t be doing either of these!',
+        'strings' => '[Strings](' . self::MANUAL_URL_BASE . '/language.types.string.php)',
+        'types' => '[Type juggling](' . self::MANUAL_URL_BASE . '/language.types.type-juggling.php)',
+        'traits' => '[Traits](' . self::MANUAL_URL_BASE . '/language.oop5.traits.php)',
+        'tags' => '[PHP tags](' . self::MANUAL_URL_BASE . '/language.basic-syntax.phptags.php)',
+        'vars' => '[Variables](' . self::MANUAL_URL_BASE . '/language.variables.php)',
+        'use' => 'The `use` keyword is used to [import symbols from another namespace](' . self::MANUAL_URL_BASE . '/language.namespaces.importing.php)'
+               . ' and to import a [trait](' . self::MANUAL_URL_BASE . '/language.oop5.traits.php) into a class.',
+        'variables' => '[Variables](' . self::MANUAL_URL_BASE . '/language.variables.php)',
+        'yeild' => '[Generators](' . self::MANUAL_URL_BASE . '/language.generators.php)',
+        'yield' => '[Generators](' . self::MANUAL_URL_BASE . '/language.generators.php)',
+    ];
+
     public function __construct(ChatClient $chatClient) {
         $this->chatClient = $chatClient;
     }
@@ -39,7 +109,17 @@ class Docs implements Plugin
     }
 
     private function getResult(Command $message): \Generator {
-        $pattern = str_replace('::', '.', implode(' ', $message->getParameters()));
+        $pattern = strtolower(implode(' ', $message->getParameters()));
+
+        foreach ([$pattern, '$' . $pattern, $pattern . 's', $pattern . 'ing'] as $candidate) {
+            if (isset($this->specialCases[$candidate])) {
+                yield from $this->chatClient->postMessage(
+                    $this->specialCases[$candidate]
+                );
+
+                return;
+            }
+        }
 
         if (substr($pattern, 0, 6) === "mysql_") {
             yield from $this->chatClient->postMessage(
@@ -49,6 +129,7 @@ class Docs implements Plugin
             return;
         }
 
+        $pattern = str_replace('::', '.', $pattern);
         $url = self::LOOKUP_URL_BASE . rawurlencode($pattern);
 
         /** @var Response $response */
