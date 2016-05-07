55d853a15e15b7175d8d8051c22324cf2df4d092
Pieter Hordijk <info@pieterhordijk.com>
2016-05-07 16:04:07 +0200

    Added logger tests

---
+++ tests/src/Log/BaseLoggerTest.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace Room11\Jeeves\Tests\Log;
+
+use Room11\Jeeves\Log\BaseLogger;
+use Room11\Jeeves\Log\Logger;
+use Room11\Jeeves\Log\Level;
+
+class BaseLoggerTest extends \PHPUnit_Framework_TestCase
+{
+    private $logger;
+
+    public function setUp()
+    {
+        $this->logger = new class(Level::EVENT) extends BaseLogger {
+            public function log(int $level, string $message, $extraData = null) { }
+
+            public function testMeetsLogLevel(int $messageLogLevel)
+            {
+                return $this->meetsLogLevel($messageLogLevel);
+            }
+        };
+    }
+
+    public function testImplementsCorrectInterface()
+    {
+        $this->assertInstanceof(Logger::class, $this->logger);
+    }
+
+    public function testMeetsLogLevelDoesntMeet()
+    {
+        $this->assertFalse($this->logger->testMeetsLoglevel(Level::ERROR));
+    }
+
+    public function testMeetsLogLevelMeets()
+    {
+        $this->assertTrue($this->logger->testMeetsLoglevel(Level::EVENT));
+    }
+}
