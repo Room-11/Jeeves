60a3f34abfe8701e0d9ad60c176af73e151a0260
Chris Wright <daverandom@php.net>
2016-05-11 15:15:24 +0100

    Update Giphy implementation to v2 API

--- src/Chat/Plugin/Giphy.php
+++ src/Chat/Plugin/Giphy.php
@@ -2,13 +2,19 @@
 
 namespace Room11\Jeeves\Chat\Plugin;
 
+use Amp\Artax\HttpClient;
+use Amp\Artax\Response as HttpResponse;
 use Room11\Jeeves\Chat\Client\ChatClient;
-use Room11\Jeeves\Chat\Command\Command;
-use Room11\Jeeves\Chat\Command\Message;
+use Room11\Jeeves\Chat\Message\Command;
+use Room11\Jeeves\Chat\Plugin;
+use Room11\Jeeves\Chat\Plugin\Traits\CommandOnly;
+use Room11\Jeeves\Chat\PluginCommandEndpoint;
 
 class Giphy implements Plugin
 {
-    const COMMAND = 'giphy';
+    use CommandOnly;
+
+    const API_BASE_URL = 'http://api.giphy.com/';
     const PUBLIC_BETA_API_KEY = 'dc6zaTOxFJmzC';
 
     const RATING_Y = 'y';
@@ -26,20 +32,23 @@ class Giphy implements Plugin
     ];
 
     private $chatClient;
+    private $httpClient;
     private $apiKey;
     private $rating;
 
     public function __construct(
         ChatClient $chatClient,
+        HttpClient $httpClient,
         $apiKey = self::PUBLIC_BETA_API_KEY,
         $rating = self::RATING_PG13
     ) {
         $this->chatClient = $chatClient;
+        $this->httpClient = $httpClient;
         $this->apiKey = $apiKey;
         $this->setRating($rating);
     }
 
-    private function setRating($rating): void
+    private function setRating($rating) /*: void*/
     {
         if (!in_array($rating, self::VALID_RATINGS)) {
             throw new \DomainException(
@@ -54,41 +63,53 @@ class Giphy implements Plugin
         $this->rating = $rating;
     }
 
-    public function handle(Message $message): \Generator
-    {
-        if (!$this->validMessage($message)) {
-            return;
-        }
-
-        yield from $this->getResult($message);
-    }
-
-    private function validMessage(Message $message): bool
-    {
-        return $message instanceof Command
-            && $message->getCommand() === self::COMMAND
-            && $message->getParameters();
-    }
-
-    private function getResult(Message $message): \Generator
-    {
-        $response = yield from $this->chatClient->request(
-            'http://api.giphy.com/v1/gifs/random?' . http_build_query([
-                'api_key' => $this->apiKey,
-                'rating' => $this->rating,
-                'tag' => implode('%20', $message->getParameters())
-            ])
-        );
-
-        $result = json_decode($response->getBody(), true);
-
-        yield from $this->chatClient->postMessage($this->getMessage($result));
-    }
-
     private function getMessage(array $result): string
     {
         return empty($result['data'])
             ? 'Very iffy! Jeeves found no giphy :('
             : $result['data']['image_url'];
+    }
+
+    public function random(Command $command): \Generator
+    {
+        if (!$command->hasParameters()) {
+            return;
+        }
+
+        /** @var HttpResponse $response */
+        $response = yield $this->httpClient->request(
+            self::API_BASE_URL . 'v1/gifs/random?' . http_build_query([
+                'api_key' => $this->apiKey,
+                'rating' => $this->rating,
+                'tag' => implode(' ', $command->getParameters())
+            ])
+        );
+
+        $result = json_decode($response->getBody(), true);
+
+        yield from $this->chatClient->postMessage($command->getRoom(), $this->getMessage($result));
+    }
+
+    public function getName(): string
+    {
+        return 'Giphy';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Gets random gifs from Giphy and displays them in oneboxes';
+    }
+
+    public function getHelpText(array $args): string
+    {
+        // TODO: Implement getHelpText() method.
+    }
+
+    /**
+     * @return PluginCommandEndpoint[]
+     */
+    public function getCommandEndpoints(): array
+    {
+        return [new PluginCommandEndpoint('Random', [$this, 'random'], 'giphy')];
     }
 }
