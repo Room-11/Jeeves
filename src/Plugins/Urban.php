d722e49a3d8eb0a51d44ff65fba08f7c14212de8
Chris Wright <daverandom@php.net>
2016-09-02 23:50:17 +0100

    Flags rule

--- src/Plugins/Urban.php
+++ src/Plugins/Urban.php
@@ -6,6 +6,7 @@ use Amp\Artax\HttpClient;
 use Amp\Artax\Response as HttpResponse;
 use Amp\Success;
 use Room11\Jeeves\Chat\Client\ChatClient;
+use Room11\Jeeves\Chat\Client\PostFlags;
 use Room11\Jeeves\Chat\Message\Command;
 use Room11\Jeeves\System\PluginCommandEndpoint;
 
@@ -20,16 +21,9 @@ class Urban extends BasePlugin
         $this->httpClient = $httpClient;
     }
 
-    // @todo make this "global"
-    public function normalizeMessage(string $message): string
-    {
-        return trim(str_replace(["\r", "\n"], ' ', $message));
-    }
-
     private function getMessage(array $result): string
     {
-        if ($result['result_type'] === 'no_results')
-        {
+        if ($result['result_type'] === 'no_results')         {
             return 'whatchoo talkin bout willis';
         }
 
@@ -37,7 +31,7 @@ class Urban extends BasePlugin
             '[ [%s](%s) ] %s',
             trim($result['list'][0]['word']),
             $result['list'][0]['permalink'],
-            str_replace("\r\n", ' ', $result['list'][0]['definition'])
+            $result['list'][0]['definition']
         ));
     }
 
@@ -54,7 +48,7 @@ class Urban extends BasePlugin
 
         $result = json_decode($response->getBody(), true);
 
-        return $this->chatClient->postMessage($command->getRoom(), $this->getMessage($result));
+        return $this->chatClient->postMessage($command->getRoom(), $this->getMessage($result), PostFlags::TRUNCATE);
     }
 
     public function getDescription(): string
