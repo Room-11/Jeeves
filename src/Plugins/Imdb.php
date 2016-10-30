7e2d8f2fbff5b9af87a20a23e4e6b17a9a9f828c
Sean Nessworthy <bladescope@googlemail.com>
2016-10-30 13:32:36 +0000

    First pass at using the omdb API instead of IMDB.

--- src/Plugins/Imdb.php
+++ src/Plugins/Imdb.php
@@ -5,21 +5,162 @@ namespace Room11\Jeeves\Plugins;
 use Amp\Artax\HttpClient;
 use Amp\Artax\Response as HttpResponse;
 use Amp\Success;
+use Ds\Queue;
 use Room11\Jeeves\Chat\Client\ChatClient;
+use Room11\Jeeves\Chat\Entities\PostedMessage;
 use Room11\Jeeves\Chat\Message\Command;
 use Room11\Jeeves\System\PluginCommandEndpoint;
 use function Room11\DOMUtils\domdocument_load_html;
 
 class Imdb extends BasePlugin
 {
+    const OMDB_API_ENDPOINT = 'http://www.omdbapi.com/';
     private $chatClient;
-
     private $httpClient;
+    private $queue;
 
     public function __construct(ChatClient $chatClient, HttpClient $httpClient)
     {
         $this->chatClient = $chatClient;
         $this->httpClient = $httpClient;
+        $this->queue = new Queue();
+    }
+
+    public function search(Command $command): \Generator
+    {
+        if (!$command->hasParameters()) {
+            // TODO: Usage instead?
+            return new Success();
+        }
+
+        $search = $command->getText();
+
+        $params = $this->buildTitleSearchParams($search);
+
+        // Send it out.
+        /** @var HttpResponse $response */
+        $response = yield $this->httpClient->request(
+            sprintf('%s?%s', self::OMDB_API_ENDPOINT, http_build_query($params))
+        );
+
+        /** @var \stdClass $data */
+        $data = @json_decode($response->getBody());
+
+        if (!$data || $data->Response === 'False') {
+            return $this->chatClient->postMessage($command->getRoom(), 'I couldn\'t find anything for that title.');
+        }
+
+        $searchResults = [];
+        foreach ($data->Search as $searchResult) {
+            $searchResults[$searchResult->imdbID] = $searchResult;
+        }
+
+        // Only pick the top 5 results if needed.
+        $searchResults = array_slice($searchResults, 0, 5, true);
+
+        /** @var PostedMessage $chatMessage */
+        $message = yield $this->chatClient->postMessage(
+            $command->getRoom(),
+            $this->formatSearchResults($searchResults)
+        );
+
+        $requests = [];
+        // Send out multiple http requests to get film descriptions and ratings.
+        foreach ($searchResults as $id => $searchResult) {
+            $requests[$id] = sprintf(
+                '%s?%s',
+                self::OMDB_API_ENDPOINT,
+                http_build_query($this->buildTitleDescParams($id))
+            );
+        }
+
+        // Wait until all requests are done, allow failures.
+        $allRequests = \Amp\some($this->httpClient->requestMulti($requests));
+        list($errors, $responses) = yield $allRequests;
+
+        $descriptionResults = [];
+        foreach ($responses as $key => $response)
+        {
+            $responseBody = @json_decode($response->getBody());
+            if(!$responseBody || $responseBody->Response === 'False') {
+                continue;
+            }
+
+            $descriptionResults[$key] = $responseBody;
+        }
+
+        return $this->chatClient->editMessage(
+            $message,
+            $this->formatSearchResults($searchResults, $descriptionResults)
+        );
+
+    }
+
+    private function buildTitleSearchParams(string $searchString): array
+    {
+        $params = [
+            's' => $searchString,
+            'r' => 'json',
+            'type' => 'movie'
+        ];
+
+        return $params;
+    }
+
+    private function formatSearchResults(array $searchResults, array $deepResults = null) : string
+    {
+        $outputLines = [];
+
+        foreach ($searchResults as $id => $searchResult)
+        {
+            $description = '';
+            if(is_array($deepResults) && isset($deepResults[$id])) {
+                $description = sprintf(
+                    ' - %s [♥ %s | 🍅 %s]',
+                    $deepResults[$id]->Plot,
+                    $deepResults[$id]->imdbRating,
+                    $deepResults[$id]->tomatoRating
+                );
+            }
+            $outputLines[] = sprintf(
+                '%s (%d) [ %s ]%s',
+                $searchResult->Title,
+                $searchResult->Year,
+                $this->getImdbUrlById($searchResult->imdbID),
+                $description
+            );
+        }
+
+        return implode("\n", $outputLines);
+    }
+
+    private function getImdbUrlById(string $id)
+    {
+        return sprintf('http://www.imdb.com/title/%s/', $id);
+    }
+
+    private function buildTitleDescParams(string $id): array
+    {
+        $params = [
+            'i' => $id,
+            'r' => 'json',
+            'tomatoes' => 'true'
+        ];
+
+        return $params;
+    }
+
+    public function getDescription(): string
+    {
+        return 'Searches and displays IMDB entries';
+    }
+
+    /**
+     * @return PluginCommandEndpoint[]
+     */
+    public function getCommandEndpoints(): array
+    {
+        return [new PluginCommandEndpoint('Search', [$this, 'search'], 'imdb')];
     }
 
     private function getMessage(HttpResponse $response): string
@@ -41,34 +182,5 @@ class Imdb extends BasePlugin
             'http://www.imdb.com/title/' . $result->getAttribute('id'),
             $result->getElementsByTagName('description')->item(0)->textContent
         );
-    }
-
-    public function search(Command $command): \Generator
-    {
-        if (!$command->hasParameters()) {
-            return new Success();
-        }
-
-        $response = yield $this->httpClient->request(
-            'http://www.imdb.com/xml/find?xml=1&nr=1&tt=on&q=' . rawurlencode(implode(' ', $command->getParameters()))
-        );
-
-        return $this->chatClient->postMessage(
-            $command->getRoom(),
-            $this->getMessage($response)
-        );
-    }
-
-    public function getDescription(): string
-    {
-        return 'Searches and displays IMDB entries';
-    }
-
-    /**
-     * @return PluginCommandEndpoint[]
-     */
-    public function getCommandEndpoints(): array
-    {
-        return [new PluginCommandEndpoint('Search', [$this, 'search'], 'imdb')];
     }
 }
