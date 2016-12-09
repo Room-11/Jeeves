277148bcfeec08dbd83c2381192c6f9fc96006c9
Chris Wright <daverandom@php.net>
2016-12-09 16:41:36 +0000

    Make Jeeves Great Again

--- src/Chat/Client/PostFlags.php
+++ src/Chat/Client/PostFlags.php
@@ -7,8 +7,8 @@ class PostFlags
     const NONE          = 0b00000000;
     const FIXED_FONT    = 0b00000001;
     const ALLOW_PINGS   = 0b00000010;
-    const ALLOW_REPLIES = 0000000100;
-    const SINGLE_LINE   = 0000001000;
-    const TRUNCATE      = 0000011000; // truncate implies single line
-    const FORCE         = 0000100000; // override isApproved
+    const ALLOW_REPLIES = 0b00000100;
+    const SINGLE_LINE   = 0b00001000;
+    const TRUNCATE      = 0b00011000; // truncate implies single line
+    const FORCE         = 0b00100000; // override isApproved
 }
