657095ad2c5e2fbca50bd9b204537490f7727b44
James <jamesbwebdev@gmail.com>
2017-01-22 20:41:45 +0000

    Lets not use tabs

--- src/Chat/Client/PendingMessage.php
+++ src/Chat/Client/PendingMessage.php
@@ -4,35 +4,35 @@ namespace Room11\Jeeves\Chat\Client;
 
 class PendingMessage
 {
-	private $message;
-	private $commandId = null;
+    private $message;
+    private $commandId = null;
 
-	public function __construct(string $message, ?int $commandId)
-	{
-		$this->setMessage($message);
+    public function __construct(string $message, ?int $commandId)
+    {
+        $this->setMessage($message);
 
-		if (!is_null($commandId)) {
-			$this->setCommandId((int) $commandId);
-		}
-	}
+        if (!is_null($commandId)) {
+            $this->setCommandId((int) $commandId);
+        }
+    }
 
-	public function setMessage(string $message)
-	{
-		$this->message = $message;
-	}
+    public function setMessage(string $message)
+    {
+        $this->message = $message;
+    }
 
-	public function setCommandId(int $commandId)
-	{
-		$this->commandId = $commandId;
-	}
+    public function setCommandId(int $commandId)
+    {
+        $this->commandId = $commandId;
+    }
 
-	public function getMessage()
-	{
-		return $this->message;
-	}
+    public function getMessage()
+    {
+        return $this->message;
+    }
 
-	public function getCommandId()
-	{
-		return $this->commandId;
-	}
+    public function getCommandId()
+    {
+        return $this->commandId;
+    }
 }
