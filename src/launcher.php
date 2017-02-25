c9f39437b74c7161669f6cb470ff94e1211c7a96
Sean Nessworthy <sean@nessworthy.me>
2017-02-25 18:00:41 +0000

    MuteStorage is no longer a thing.

--- src/launcher.php
+++ src/launcher.php
@@ -38,12 +38,10 @@ use Room11\Jeeves\Storage\File\CommandAlias as FileCommandAliasStorage;
 use Room11\Jeeves\Storage\File\KeyValue as FileKeyValueStorage;
 use Room11\Jeeves\Storage\File\Plugin as FilePluginStorage;
 use Room11\Jeeves\Storage\File\Room as FileRoomStorage;
-use Room11\Jeeves\Storage\File\Mute as FileMuteStorage;
 use Room11\Jeeves\Storage\KeyValue as KeyValueStorage;
 use Room11\Jeeves\Storage\KeyValueFactory as KeyValueStorageFactory;
 use Room11\Jeeves\Storage\Plugin as PluginStorage;
 use Room11\Jeeves\Storage\Room as RoomStorage;
-use Room11\Jeeves\Storage\Mute as MuteStorage;
 use Room11\Jeeves\System\BuiltInActionManager;
 use Room11\Jeeves\System\Plugin;
 use Room11\Jeeves\System\PluginManager;
@@ -83,7 +81,6 @@ $injector->alias(KeyValueStorage::class, $config['storage']['keyvalue'] ?? FileK
 $injector->alias(KeyValueStorageFactory::class, ($config['storage']['keyvalue'] ?? FileKeyValueStorage::class) . 'Factory');
 $injector->alias(PluginStorage::class,   $config['storage']['plugin']   ?? FilePluginStorage::class);
 $injector->alias(RoomStorage::class,     $config['storage']['room']     ?? FileRoomStorage::class);
-$injector->alias(MuteStorage::class,     $config['storage']['mute']     ?? FileMuteStorage::class);
 
 $injector->define(BitlyClient::class, [':accessToken' => $config['bitly']['accessToken']]);
 
