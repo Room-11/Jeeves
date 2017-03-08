21dfa1637759f1016c42e133bab603e85cd5c917
James <jamesbwebdev@gmail.com>
2017-03-08 20:33:48 +0000

    Uptime test

---
+++ tests/src/Chat/BuiltIn/AbstractBuiltInTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace Room11\Jeeves\Tests\Chat\builtIn;
+
+use Room11\Jeeves\System\Plugin;
+use Room11\Jeeves\Tests\Chat\AbstractChatTest;
+
+abstract class AbstractBuiltInTest extends AbstractChatTest
+{
+    protected $builtIn;
+}
---
+++ tests/src/Chat/BuiltIn/UptimeTest.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types = 1);
+
+namespace Room11\Jeeves\Tests\Chat\BuiltIn;
+
+use Amp\Success;
+use AsyncInterop\Loop;
+use Room11\Jeeves\BuiltIn\Commands\Uptime;
+use Room11\Jeeves\Chat\Message\Command;
+use Room11\Jeeves\Chat\Room\Room;
+use Room11\Jeeves\System\BuiltInCommandInfo;
+
+class UptimeTest extends AbstractBuiltInTest
+{
+    const VALID_UPTIME_EXP = "\d \bsecond(s)?\b|\d \bminute(s)?\b|\d \bday(s)?\b|\d \bhour(s)?\b";
+
+    private $command; 
+    private $room;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        if (!defined('Room11\\Jeeves\\PROCESS_START_TIME')) {
+            define('Room11\\Jeeves\\PROCESS_START_TIME', time());
+        }
+
+        $this->builtIn = new Uptime($this->client);
+
+        $this->command = $this->getMockBuilder(Command::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->room = $this->getMockBuilder(Room::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testCommandInfo()
+    {
+        $this->assertInstanceOf(BuiltInCommandInfo::class, $this->builtIn->getCommandInfo()[0]);
+    }
+
+    public function testUptimeCommand()
+    {
+        $this->room
+            ->expects($this->exactly(5000))
+            ->method('isApproved')
+            ->will($this->returnValue(new Success(true)));
+
+        $this->client
+            ->expects($this->once())
+            ->method('postMessage')
+            ->with(
+                $this->identicalTo($this->command),
+                $this->matchingRegularExpression(self::VALID_UPTIME_EXP)
+            );
+
+        yield $this->builtIn->handleCommand($this->command);
+    }
+
+    public function testUnApprovedUptimeCommand()
+    {
+        $this->room
+            ->expects($this->once())
+            ->method('isApproved')
+            ->will($this->returnValue(false));
+
+        $response = yield $this->builtIn->handleCommand($this->command);
+
+        $this->assertNull($response);
+    }
+}
