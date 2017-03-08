3f4671c1c7eafb3572791c5c5fb829d17b970698
Pieter Hordijk <info@pieterhordijk.com>
2017-03-08 13:29:10 +0100

    Refactor layout of startup scripts

---
+++ src/bootstrap.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types = 1);
+
+namespace Room11\Jeeves;
+
+const DATA_BASE_DIR = __DIR__ . '/../data';
+define(__NAMESPACE__ . '\\PROCESS_START_TIME', time());
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../version.php';
