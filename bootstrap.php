aa6d711c13ef65d0ed525cc3d23fcb39552f2658
Chris Wright <daverandom@php.net>
2017-03-19 15:04:06 +0000

    Fix APP_BASE definition properly...

--- bootstrap.php
+++ bootstrap.php
@@ -3,11 +3,11 @@
 namespace Room11\Jeeves;
 
 const GITHUB_PROJECT_URL = 'https://github.com/Room-11/Jeeves';
-const DATA_BASE_DIR = __DIR__ . '/../data';
+const APP_BASE = __DIR__;
+const DATA_BASE_DIR = APP_BASE . '/data';
 
 define(__NAMESPACE__ . '\\PROCESS_START_TIME', time());
 define(__NAMESPACE__ . '\\IS_WINDOWS', stripos(PHP_OS, 'win') === 0);
-define(__NAMESPACE__ . '\\APP_BASE', __DIR__);
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/version.php';
