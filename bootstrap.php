d428453ecdd973fc63fa8081dfcc205b9e36d472
Chris Wright <daverandom@php.net>
2017-03-19 15:02:20 +0000

    Fix APP_BASE definition

--- bootstrap.php
+++ bootstrap.php
@@ -7,7 +7,7 @@ const DATA_BASE_DIR = __DIR__ . '/../data';
 
 define(__NAMESPACE__ . '\\PROCESS_START_TIME', time());
 define(__NAMESPACE__ . '\\IS_WINDOWS', stripos(PHP_OS, 'win') === 0);
-define(__NAMESPACE__ . '\\APP_BASE', realpath(__DIR__ . '/Jeeves'));
+define(__NAMESPACE__ . '\\APP_BASE', __DIR__);
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/version.php';
