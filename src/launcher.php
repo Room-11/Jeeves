ef7cc91f8526175ab51d260558dd68f90b1df5f8
Saitama Sama <ping@gourab.me>
2017-10-14 21:21:00 +0530

    Add Weather Plugin

--- src/launcher.php
+++ src/launcher.php
@@ -48,6 +48,7 @@ use Room11\OpenId\Password as OpenIdPassword;
 use Room11\StackChat\Auth\CredentialManager;
 use Room11\StackChat\Room\Room;
 use Symfony\Component\Yaml\Yaml;
+use Room11\Jeeves\Plugins\Weather as WeatherPlugin;
 use function Amp\onError;
 use function Amp\run;
 
@@ -100,6 +101,10 @@ $injector->define(GithubIssueCredentials::class, [
     ':username' => $config['github']['username'] ?? '',
     ':password' => $config['github']['password'] ?? '',
     ':token'    => $config['github']['token'] ?? ''
+]);
+
+$injector->define(WeatherPlugin::class, [
+    ':apiKey' => $config['openweathermap']['api-key']
 ]);
 
 $injector->define(WebSocketEventDispatcherFactory::class, [
