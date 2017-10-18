bcba66071d62919f1d3814a97fdb9551beb1546a
Pieter Hordijk <info@pieterhordijk.com>
2017-10-18 03:00:54 +0200

    Make reboot admin only. This means any admin sadly.

--- src/BuiltIn/Commands/Reboot.php
+++ src/BuiltIn/Commands/Reboot.php
@@ -5,6 +5,7 @@ namespace Room11\Jeeves\BuiltIn\Commands;
 use Amp\Promise;
 use function Amp\resolve;
 use Room11\Jeeves\Chat\Command as CommandMessage;
+use Room11\Jeeves\Storage\Admin as AdminStorage;
 use Room11\Jeeves\System\BuiltInCommand;
 use Room11\Jeeves\System\BuiltInCommandInfo;
 use Room11\StackChat\Client\Client as ChatClient;
@@ -13,14 +14,21 @@ class Reboot implements BuiltInCommand
 {
     private $chatClient;
 
-    public function __construct(ChatClient $chatClient)
+    private $adminStorage;
+
+    public function __construct(ChatClient $chatClient, AdminStorage $adminStorage)
     {
-        $this->chatClient = $chatClient;
+        $this->chatClient   = $chatClient;
+        $this->adminStorage = $adminStorage;
     }
 
     public function handleCommand(CommandMessage $command): Promise
     {
         return resolve(function() use ($command) {
+            if (!yield $this->adminStorage->isAdmin($command->getRoom(), $command->getUserId())) {
+                return $this->chatClient->postReply($command, "I'm sorry Dave, I'm afraid I can't do that");
+            }
+
             $message = yield $this->chatClient->postMessage($command, 'Restarting in 10 seconds.');
 
             \Amp\once(function() use ($message) {
